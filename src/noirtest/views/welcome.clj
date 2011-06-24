25ba3c18aa2d90b16f7e6024e4f298317611a4f9
youz <citrus.yubeshi@gmail.com>
2011-06-24 22:34:57 +0900

    http status test

--- src/noirtest/views/welcome.clj
+++ src/noirtest/views/welcome.clj
@@ -1,15 +1,93 @@
 ;;; -*- coding: utf8n -*-
 (ns noirtest.views.welcome
-  (:require [noirtest.views.common :as common])
-;            [noir.content.pages :as pages])
+  (:require [noirtest.views.common :as common]
+            [noir.response :as resp])
   (use noir.core
        hiccup.core
        hiccup.page-helpers))
 
 
-(defpage "/*" []
+(def location
+     "http://quiet-fog-825.herokuapp.com/")
+
+(def statuses
+     {
+      200 "OK"
+      201 "Created"
+      202 "Accepted"
+      203 "Non-Authoritative Information"
+      204 {:desc "No Content"}
+      205 {:desc "Reset Content"}
+      206 "Partial Content"
+      300 "Multiple Choices"
+      301 {:headers {"Location" location}
+	   :body "Moved Permanently"}
+      302 {:headers {"Location" location}
+	   :body "Found"}
+      303 {:headers {"Location" location}
+	   :body "See Other"}
+      304 {:desc "Not Modified"}
+      305 {:headers {"Location" location}
+	   :body "Use Proxy"}
+      306 "Unused"
+      307 {:headers {"Location" location}
+	   :body "Temporary Redirect"}
+      400 "Bad Request"
+      401 {:headers {"Www-Authenticate" "Basic realm=\"Fake Realm\""}
+	   :body "Unauthorized"}
+      402 "Payment Required"
+      403 "Forbidden"
+      404 "Not Found"
+      405 "Method Not Allowed"
+      406 "Not Acceptable"
+      407 {:headers {"Proxy-Authenticate" "Basic realm=\"Fake Realm\""}
+	   :body "Proxy Authentication Required"}
+      408 "Request Timeout"
+      409 "Conflict410 Gone"
+      411 "Length Required"
+      412 "Precondition Required"
+      413 "Request Entity Too Large"
+      414 "Request-URI Too Long"
+      415 "Unsupported Media Type"
+      416 "Requested Range Not Satisfiable"
+      417 "Expectation Failed"
+      418 "I'm a Teapot"
+      500 "Internal Server Error"
+      501 "Not Implemented"
+      502 "Bad Gateway"
+      503 "Service Unavailable"
+      504 "Gateway Timeout"
+      505 "HTTP Version Not Supported"
+      })
+
+(defn make-res [code]
+  (let [res (statuses code :unknown)]
+    (cond (= res :unknown) (make-res 400)
+	  (string? res) {:status code :body res}
+	  (map? res)
+	  (let [head (res :headers)
+		body (res :body)]
+	    (conj {:status code :body body}
+		  (if head {:headers head}))))))
+
+(defn site-index []
   {
    :headers {"Content-Type" "text/html; charset=UTF-8"}
-   :body   (common/layout
-	    [:p "実際ハヤイ"])
+   :body (common/layout
+	  [:p "すてーたす てすと"]
+	  [:ul
+	   (map (fn [code]
+		  (let [v (statuses code)
+			desc (if (string? v) v (v :desc (v :body)))]
+		    [:li (link-to (str "/" code) (str code " " desc))]))
+		(sort (keys statuses)))
+	   ])
    })
+
+(defpage "/" []
+  (site-index))
+
+(defpage "/:code" {:keys [code]}
+  (if (every? #(<= 48 (int %) 57) code)
+    (make-res (read-string code))
+    (make-res 400)))
