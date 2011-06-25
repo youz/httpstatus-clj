d36a2774936f50fefdba43c50f3d83b854b5f730
youz <citrus.yubeshi@gmail.com>
2011-06-25 11:06:41 +0900

    separate default-style

--- src/noirtest/views/common.clj
+++ src/noirtest/views/common.clj
@@ -7,26 +7,28 @@
        hiccup.core
        hiccup.page-helpers))
 
+(def style
+     [:style {:type "text/css"}
+      (css
+       (rule "body"
+	     dark-background
+	     :padding [:60px :80px]
+	     :font-family "serif")
+       (rule "h1"
+	     emphasis
+	     :font-size :20px
+	     :margin-bottom :20px)
+       (rule "a:link, a:visited"
+	     :text-decoration :none
+	     :color :#aae))
+      ])
+
 (defpartial layout [& content]
   (html5
    [:head
     [:title "のわーるテスト"]
     (include-css "/css/reset.css")
-    [:style {:type "text/css"}
-     (css
-      (rule "body"
-	    dark-background
-	    light-text
-	    :padding [:60px :80px]
-	    :font-family "serif")
-      (rule "h1"
-	    emphasis
-	    :font-size :20px
-	    :margin-bottom :20px)
-      (rule "a:link, a:visited"
-	    :text-decoration :none
-	    :color :#aae))
-     ]]
+    style]
    [:body
     [:div#wrapper
      content]]))
