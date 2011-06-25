762f150a0c0bee3d975044e56c3d18a1a850f4a4
youz <citrus.yubeshi@gmail.com>
2011-06-25 11:07:04 +0900

    changed title

--- src/noirtest/views/welcome.clj
+++ src/noirtest/views/welcome.clj
@@ -71,7 +71,7 @@
   {
    :headers {"Content-Type" "text/html; charset=UTF-8"}
    :body (common/layout
-          [:h1 "HTTP Statuses"]
+          [:h1 "HTTP Statuses Test"]
           [:ul
            (map (fn [code]
                   (let [v (statuses code)
